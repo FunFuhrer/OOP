175c47ebfa688dbbedb29140549561048a812736
FunFuhrer <49659199+FunFuhrer@users.noreply.github.com>
2019-04-26 13:10:07 +0300

    Lab4

---
+++ Lab4.4.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Vector {
+	double x, y, z;
+public:
+	Vector(double x, double y, double z)
+	{
+		cout << "Made " << this << endl;
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	Vector()
+	{
+		cout << "Made " << this << endl;
+		this->x = 0;
+		this->y = 0;
+		this->z = 0;
+	}
+	Vector(const Vector &other)
+	{
+		cout << "Copy " << this << endl;
+		this->x = other.x;
+		this->y = other.y;
+		this->z = other.z;
+	}
+	~Vector() {
+		cout << "destruction " << this << endl;
+	}
+	void Print()
+	{
+		cout << x << "," << y << "," << z << endl;
+	}
+
+	double module()
+	{
+		return sqrt(x*x + y * y + z * z);
+	}
+
+	friend Vector sum(Vector a, Vector b)
+	{
+		Vector c;
+		c.x = a.x + b.x;
+		c.y = a.y + b.y;
+		c.z = a.z + b.z;
+		return c;
+	}
+	friend Vector dif(Vector a, Vector b)
+	{
+		Vector c;
+		c.x = a.x - b.x;
+		c.y = a.y - b.y;
+		c.z = a.z - b.z;
+		return c;
+	}
+};
+int main() {
+	setlocale(0, "rus");
+	Vector a(1, 4, 8);
+	Vector b(1, 3, 7);
+	Vector c;
+	Vector d;
+	c = sum(a, b);
+	d = dif(a, b);
+	cout << "Modul' :" << a.module() << "   " << b.module() << endl;
+	cout << "Sum :";
+	c.Print();
+	cout << "Dif :";
+	d.Print();
+	Vector m(a);
+	system("pause");
+	return 0;
+}
